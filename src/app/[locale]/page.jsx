c9028009e53f5df14ea1175f635c118a9cb2ceed
Yussuf Nergiz <yussufnergiz@Yussufs-MBP.lan>
2024-10-30 00:34:06 +0500

    added tally wait list

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,4 +1,5 @@
 import { getIntl } from "../../lib/intl";
+import Script from "next/script";
 
 export const runtime = "edge";
 
@@ -7,9 +8,16 @@ export default async function Home({ params }) {
     const intl = await getIntl(locale);
 
     return (
-        <div>
-            <h1>{intl.formatMessage({ id: "coming-soon.title" })}</h1>
-            <h2>{intl.formatMessage({ id: "coming-soon.subtitle" })}</h2>
+        <div style={{height: "100vh"}}>
+            {/*<h1>{intl.formatMessage({ id: "coming-soon.title" })}</h1>*/}
+            {/*<h2>{intl.formatMessage({ id: "coming-soon.subtitle" })}</h2>*/}
+            <iframe data-tally-src="https://tally.so/r/mDvvXN?transparentBackground=1" width="100%" height="100%"
+                    frameBorder="0" marginHeight="0" marginWidth="0" title="Join the Waitlist..."></iframe>
+
+            <Script
+                id="tally-js"
+                src="https://tally.so/widgets/embed.js"
+            />
         </div>
     );
 }

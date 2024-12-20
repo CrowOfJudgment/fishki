e62a48b7e7313b686bc0443321b197a92d1aed09
Yussuf Nergiz <yusufyn04@gmail.com>
2024-12-20 01:40:24 +0100

    created landing page

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,13 +1,22 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Script from "next/script";
+import {Header} from "../../sections/Header";
+import {Hero} from "../../sections/Hero";
+import {ProductShowcase} from "../../sections/ProductShowcase";
+import {CallToAction} from "../../sections/CallToAction";
+import {Footer} from "../../sections/Footer";
+import {Pricing} from "../../sections/Pricing";
+import {getIntl} from "../../lib/intl";
+import {i18n} from "../../../i18n-config";
 
 export const runtime = "edge";
 
 export default function Home({ params }) {
     const resolvedParams = React.use(params);
     const [messages, setMessages] = useState(null);
+    const [locale, setLocale] = useState(i18n.defaultLocale);
+    const [intl, setIntl] = useState(null);
 
     useEffect(() => {
         async function loadMessages() {
@@ -22,22 +31,43 @@ export default function Home({ params }) {
         loadMessages();
     }, [resolvedParams.locale]);
 
-    return (
-        <div style={{height: "100vh"}}>
-            <iframe
-                data-tally-src="https://tally.so/r/mDvvXN?transparentBackground=1"
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                marginHeight="0"
-                marginWidth="0"
-                title="Join the Waitlist..."
-            ></iframe>
+    useEffect(() => {
+        const fetchIntl = async () => {
+            const intlInstance = await getIntl(locale);
+            setIntl(intlInstance);
+        };
+        fetchIntl();
+    }, [locale]);
 
-            <Script
-                id="tally-js"
-                src="https://tally.so/widgets/embed.js"
-            />
-        </div>
+    const changeLocale = (newLocale) => {
+        setLocale(newLocale);
+        document.cookie = `NEXT_LOCALE=${newLocale}; path=/`;
+    };
+
+    return (
+        <>
+            <Header locale={locale} onLocaleChange={changeLocale} intl={intl}/>
+            <Hero intl={intl}/>
+            {/*<LogoTicker />*/}
+            <ProductShowcase intl={intl}/>
+            <Pricing intl={intl}/>
+            {/*<Testimonials />*/}
+            <CallToAction intl={intl}/>
+            {/*<div>*/}
+            {/*    /!* Calendly Inline Widget *!/*/}
+            {/*    <div*/}
+            {/*        className="calendly-inline-widget"*/}
+            {/*        data-url="https://calendly.com/tablescan-saas"*/}
+            {/*        style={{minWidth: '320px', height: '700px'}}*/}
+            {/*    ></div>*/}
+
+            {/*    /!* Calendly Script *!/*/}
+            {/*    <Script*/}
+            {/*        src="https://assets.calendly.com/assets/external/widget.js"*/}
+            {/*        strategy="lazyOnload"*/}
+            {/*    />*/}
+            {/*</div>*/}
+            <Footer/>
+        </>
     );
 }

86c62722cf41f4d17b75e1566e87ab0209491b20
Yussuf Nergiz <yusufyn04@gmail.com>
2025-01-12 16:43:47 +0100

    added posthog for analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import {twMerge} from "tailwind-merge";
 import { DM_Sans } from "next/font/google";
+import { CSPostHogProvider } from './providers'
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
@@ -12,9 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+            <CSPostHogProvider>
+                <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
                 {children}
-            </body>
+                </body>
+            </CSPostHogProvider>
         </html>
     );
 }
